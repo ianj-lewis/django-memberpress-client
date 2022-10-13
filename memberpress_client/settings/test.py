966bafc398dd1cc590ebca0d9fe6b4a8471d6d10
lpm0073 <lpm0073@gmail.com>
2022-10-13 13:55:03 -0500

    setup .env to store test credentials

--- memberpress_client/settings/test.py
+++ memberpress_client/settings/test.py
@@ -1,4 +1,9 @@
-MEMBERPRESS_API_KEY = "set-me-please"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MEMBERPRESS_API_KEY = os.getenv("MEMBERPRESS_API_KEY")
 MEMBERPRESS_API_KEY_NAME = "MEMBERPRESS-API-KEY"
 MEMBERPRESS_API_BASE_URL = "https://stepwisemath.ai/"
 MEMBERPRESS_CACHE_EXPIRATION = 300

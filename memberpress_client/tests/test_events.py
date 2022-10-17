803f5ca8111677ff3d43b5a18102a2074ca833c3
lpm0073 <lpm0073@gmail.com>
2022-10-16 20:53:23 -0500

    refactor

--- memberpress_client/tests/test_events.py
+++ memberpress_client/tests/test_events.py
@@ -1,16 +1,12 @@
 # python stuff
-from email.policy import strict
 import os
 import io
 import unittest
 import json
-from datetime import datetime
-from requests import request
 
 
 # our testing code starts here
 # -----------------------------------------------------------------------------
-from memberpress_client.member import Member  # noqa: E402
 from memberpress_client.constants import MemberpressEvents
 from memberpress_client.event import get_event
 
@@ -18,70 +14,63 @@ from memberpress_client.event import get_event
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
-def load_json_file(test_file):
+def load_json(test_file):
     test_file = test_file + ".json"
-    with io.open(os.path.join(HERE, "data", test_file), "rt", encoding="utf8") as f:
+    with io.open(os.path.join(HERE, "data", "events", test_file), "rt", encoding="utf8") as f:
         return json.loads(f.read(), strict=False)
 
 
-# test data
-# -----------------------------------------------------------------------------
-
-
 class TestMember(unittest.TestCase):
-
-    PATH = "./data/events/"
-
     def test_1_valid_dicts(self):
-        def validate(data_dict: dict):
+        def validate(event_str: str):
+            data_dict = load_json(event_str)
             event = get_event(data_dict)
-            event_str = data_dict.get("event")
             self.assertEqual(event.is_valid, True)
             self.assertEqual(event.event, event_str)
 
-        validate(load_json_file(self.PATH + MemberpressEvents.AFTER_CC_EXPIRES_REMINDER))
-        validate(load_json_file(self.PATH + MemberpressEvents.AFTER_CC_EXPIRES_REMINDER))
-        validate(load_json_file(self.PATH + MemberpressEvents.AFTER_MEMBER_SIGNUP_REMINDER))
-        validate(load_json_file(self.PATH + MemberpressEvents.AFTER_SIGNUP_ABANDONED_REMINDER))
-        validate(load_json_file(self.PATH + MemberpressEvents.AFTER_SUB_EXPIRES_REMINDER))
-        validate(load_json_file(self.PATH + MemberpressEvents.BEFORE_CC_EXPIRES_REMINDER))
-        validate(load_json_file(self.PATH + MemberpressEvents.BEFORE_SUB_EXPIRES_REMINDER))
-        validate(load_json_file(self.PATH + MemberpressEvents.BEFORE_SUB_RENEWS_REMINDER))
-        validate(load_json_file(self.PATH + MemberpressEvents.BEFORE_SUB_TRIAL_ENDS))
-        validate(load_json_file(self.PATH + MemberpressEvents.LOGIN))
-        validate(load_json_file(self.PATH + MemberpressEvents.MEMBER_ACCOUNT_UPDATED))
-        validate(load_json_file(self.PATH + MemberpressEvents.MEMBER_ADDED))
-        validate(load_json_file(self.PATH + MemberpressEvents.MEMBER_DELETED))
-        validate(load_json_file(self.PATH + MemberpressEvents.MEMBER_SIGNUP_COMPLETED))
-        validate(load_json_file(self.PATH + MemberpressEvents.MPCA_COURSE_COMPLETED))
-        validate(load_json_file(self.PATH + MemberpressEvents.MPCA_COURSE_STARTED))
-        validate(load_json_file(self.PATH + MemberpressEvents.MPCA_LESSON_COMPLETED))
-        validate(load_json_file(self.PATH + MemberpressEvents.MPCA_LESSON_STARTED))
-        validate(load_json_file(self.PATH + MemberpressEvents.MPCA_QUIZ_ATTEMPT_COMPLETED))
-        validate(load_json_file(self.PATH + MemberpressEvents.NON_RECURRING_TRANSACTION_COMPLETED))
-        validate(load_json_file(self.PATH + MemberpressEvents.NON_RECURRING_TRANSACTION_EXPIRED))
-        validate(load_json_file(self.PATH + MemberpressEvents.OFFLINE_PAYMENT_COMPLETE))
-        validate(load_json_file(self.PATH + MemberpressEvents.OFFLINE_PAYMENT_PENDING))
-        validate(load_json_file(self.PATH + MemberpressEvents.OFFLINE_PAYMENT_REFUNDED))
-        validate(load_json_file(self.PATH + MemberpressEvents.RECURRING_TRANSACTION_COMPLETED))
-        validate(load_json_file(self.PATH + MemberpressEvents.RECURRING_TRANSACTION_EXPIRED))
-        validate(load_json_file(self.PATH + MemberpressEvents.RECURRING_TRANSACTION_FAILED))
-        validate(load_json_file(self.PATH + MemberpressEvents.RENEWAL_TRANSACTION_COMPLETED))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUB_ACCOUNT_ADDED))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUB_ACCOUNT_REMOVED))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_CREATED))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_DOWNGRADED_TO_ONE_TIME))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_DOWNGRADED_TO_RECURRING))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_DOWNGRADED))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_EXPIRED))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_PAUSED))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_RESUMED))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_STOPPED))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_UPGRADED_TO_ONE_TIME))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_UPGRADED_TO_RECURRING))
-        validate(load_json_file(self.PATH + MemberpressEvents.SUBSCRIPTION_UPGRADED))
-        validate(load_json_file(self.PATH + MemberpressEvents.TRANSACTION_COMPLETED))
-        validate(load_json_file(self.PATH + MemberpressEvents.TRANSACTION_EXPIRED))
-        validate(load_json_file(self.PATH + MemberpressEvents.TRANSACTION_FAILED))
-        validate(load_json_file(self.PATH + MemberpressEvents.TRANSACTION_REFUNDED))
-        validate(load_json_file(self.PATH + MemberpressEvents.UNIDENTIFIED_EVENT))
+        validate(MemberpressEvents.AFTER_CC_EXPIRES_REMINDER)
+        validate(MemberpressEvents.AFTER_CC_EXPIRES_REMINDER)
+        validate(MemberpressEvents.AFTER_MEMBER_SIGNUP_REMINDER)
+        validate(MemberpressEvents.AFTER_SIGNUP_ABANDONED_REMINDER)
+        validate(MemberpressEvents.AFTER_SUB_EXPIRES_REMINDER)
+        validate(MemberpressEvents.BEFORE_CC_EXPIRES_REMINDER)
+        validate(MemberpressEvents.BEFORE_SUB_EXPIRES_REMINDER)
+        validate(MemberpressEvents.BEFORE_SUB_RENEWS_REMINDER)
+        validate(MemberpressEvents.BEFORE_SUB_TRIAL_ENDS)
+        validate(MemberpressEvents.LOGIN)
+        validate(MemberpressEvents.MEMBER_ACCOUNT_UPDATED)
+        validate(MemberpressEvents.MEMBER_ADDED)
+        validate(MemberpressEvents.MEMBER_DELETED)
+        validate(MemberpressEvents.MEMBER_SIGNUP_COMPLETED)
+        validate(MemberpressEvents.MPCA_COURSE_COMPLETED)
+        validate(MemberpressEvents.MPCA_COURSE_STARTED)
+        validate(MemberpressEvents.MPCA_LESSON_COMPLETED)
+        validate(MemberpressEvents.MPCA_LESSON_STARTED)
+        validate(MemberpressEvents.MPCA_QUIZ_ATTEMPT_COMPLETED)
+        validate(MemberpressEvents.NON_RECURRING_TRANSACTION_COMPLETED)
+        validate(MemberpressEvents.NON_RECURRING_TRANSACTION_EXPIRED)
+        validate(MemberpressEvents.OFFLINE_PAYMENT_COMPLETE)
+        validate(MemberpressEvents.OFFLINE_PAYMENT_PENDING)
+        validate(MemberpressEvents.OFFLINE_PAYMENT_REFUNDED)
+        validate(MemberpressEvents.RECURRING_TRANSACTION_COMPLETED)
+        validate(MemberpressEvents.RECURRING_TRANSACTION_EXPIRED)
+        validate(MemberpressEvents.RECURRING_TRANSACTION_FAILED)
+        validate(MemberpressEvents.RENEWAL_TRANSACTION_COMPLETED)
+        validate(MemberpressEvents.SUB_ACCOUNT_ADDED)
+        validate(MemberpressEvents.SUB_ACCOUNT_REMOVED)
+        validate(MemberpressEvents.SUBSCRIPTION_CREATED)
+        validate(MemberpressEvents.SUBSCRIPTION_DOWNGRADED_TO_ONE_TIME)
+        validate(MemberpressEvents.SUBSCRIPTION_DOWNGRADED_TO_RECURRING)
+        validate(MemberpressEvents.SUBSCRIPTION_DOWNGRADED)
+        validate(MemberpressEvents.SUBSCRIPTION_EXPIRED)
+        validate(MemberpressEvents.SUBSCRIPTION_PAUSED)
+        validate(MemberpressEvents.SUBSCRIPTION_RESUMED)
+        validate(MemberpressEvents.SUBSCRIPTION_STOPPED)
+        validate(MemberpressEvents.SUBSCRIPTION_UPGRADED_TO_ONE_TIME)
+        validate(MemberpressEvents.SUBSCRIPTION_UPGRADED_TO_RECURRING)
+        validate(MemberpressEvents.SUBSCRIPTION_UPGRADED)
+        validate(MemberpressEvents.TRANSACTION_COMPLETED)
+        validate(MemberpressEvents.TRANSACTION_EXPIRED)
+        validate(MemberpressEvents.TRANSACTION_FAILED)
+        validate(MemberpressEvents.TRANSACTION_REFUNDED)
+        validate(MemberpressEvents.UNIDENTIFIED_EVENT)

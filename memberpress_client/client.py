04115445893d9bae6b8b197da638d0e319165439
Travis McKinney <tmckinney@mrionline.com>
2023-03-28 19:00:51 -0400

    cache key should include params

--- memberpress_client/client.py
+++ memberpress_client/client.py
@@ -71,7 +71,7 @@ class MemberpressAPIClient(Memberpress):
     @request_manager
     def get(self, path, params=None, operation="") -> json:
         url = self.get_url(path)
-        cache_key = f"MemberpressAPIClient.get:{url}"
+        cache_key = f"MemberpressAPIClient.get:{url}:{params}"
         response = cache.get(cache_key)
 
         if not response and not self.locked:
@@ -98,7 +98,7 @@ class MemberpressAPIClient(Memberpress):
             # caching results iff response is a valid json object.
             cache.set(cache_key, response, settings.MEMBERPRESS_CACHE_EXPIRATION)
             self.unlock()
-        return response
+        return response    
 
     def is_valid_dict(self, response, qc_keys) -> bool:
         if not type(response) == dict:
